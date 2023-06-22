aec44358236de5b012ae5453512c0e13bd7062d1
Anshu Ranjan <44924685+anshnee@users.noreply.github.com>
2023-06-21 21:45:02 -0500

    Terraform script for new VM creation on Azure.

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default = "eastus"
+}
+
+variable "username" {
+  
+}
+
+variable "password"{
+  
+}
